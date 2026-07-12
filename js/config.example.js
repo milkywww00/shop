b3c5d34ed0b98f989202ab3e10784c69997c8937
milkywww00 <milkywww00@gmail.com>
2026-07-12 15:01:58 +0900

    수정

---
+++ js/config.example.js
@@ -0,0 +1,10 @@
+//  이 파일의 이름을 'config.js'로 변경한 뒤, 본인의 Firebase 설정 값을 입력해주세요
+
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY_HERE",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.firebasestorage.app",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
